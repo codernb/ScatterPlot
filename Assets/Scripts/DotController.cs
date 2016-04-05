0b8ce589a7ac21844f34f36633855e0af00b0bad
codernb <cyril.halmo@students.fhnw.ch>
2016-04-05 18:34:13 +0200

    it's something

---
+++ Assets/Scripts/DotController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DotController : MonoBehaviour
+{
+
+    public float[] Values;
+
+    public void SetAxis(int axis, int i)
+    {
+        var temp = transform.position;
+        switch (axis) {
+        case 0:
+            temp.x = Values[i];
+            break;
+        case 1:
+            temp.y = Values[i];
+            break;
+        case 2:
+            temp.z = Values[i];
+            break;
+        }
+        transform.position = temp;
+    }
+
+}
---
+++ Assets/Scripts/PlotController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEditor;
+using System.Collections.Generic;
+
+public class PlotController : MonoBehaviour
+{
+
+    public GameObject Dot;
+
+    private char[] newLineDelimiter = new char[] {'\n'};
+    private char[] commaDelimiter = new char[] {','};
+
+    private string[] axisKeys;
+    private int[] selectedAxisKeys = new int[3];
+
+    private List<GameObject> dots = new List<GameObject>();
+
+    private int axisToSelect = -1;
+
+    public void SelectData()
+    {
+        foreach (var d in dots) {
+            Destroy(d);
+            dots.Remove(d);
+        }
+        var filePath = EditorUtility.OpenFilePanel("Select *.data File", "", "data");
+        if (filePath.Length == 0)
+            return;
+        var text = File.ReadAllText(filePath);
+        var lines = text.Split(newLineDelimiter);
+        axisKeys = lines[0].Split(commaDelimiter);
+        for (int i = 1; i < lines.Length; i++) {
+            var values = lines[i].Split(commaDelimiter);
+            var vals = new float[values.Length];
+            for (int j = 0; j < values.Length; j++)
+                if (values[j].Length > 0) {
+                    vals[j] = float.Parse(values[j]);
+                    GameObject dot = (GameObject) Instantiate(Dot);
+                    dot.GetComponent<DotController>().Values = vals;
+                    dots.Add(dot);
+                }
+        }
+    }
+
+    public void SelectXAxis()
+    {
+        if (axisKeys == null)
+            return;
+        axisToSelect = 0;
+    }
+    
+    public void SelectYAxis()
+    {
+        if (axisKeys == null)
+            return;
+        axisToSelect = 1;
+    }
+    
+    public void SelectZAxis()
+    {
+        if (axisKeys == null)
+            return;
+        axisToSelect = 2;
+    }
+
+    void OnGUI()
+    {
+        if (axisToSelect > -1) {
+            var selection = GUI.SelectionGrid(new Rect(10, 10, 600, 300), -1, axisKeys, 4);
+            selectedAxisKeys[axisToSelect] = selection;
+            if (selection > -1) {
+                foreach (var d in dots)
+                    d.GetComponent<DotController>().SetAxis(axisToSelect, selection);
+                axisToSelect = -1;
+            }
+        }
+    }
+
+}
